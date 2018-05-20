b52e89b9df9c95649cefd76963fbb5c0cad5cf27
Isulew <netcookies@gmail.com>
2018-05-20 13:10:55 +0800

    Fix js errors

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -269,7 +269,6 @@ export function watch(){
 // Gulp: bring them all together
 let build = gulp.parallel(sass, html, fonts, minifyImg, minifyJs, jsCombiner);
 const dev = gulp.series(clean, svg, build, gulp.parallel(liveReload, watch));
-const dist = gulp.series(clean, svg, build);
 
-export dist;
+export const dist = gulp.series(clean, svg, build);
 export default dev;
